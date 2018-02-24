3545c1eb30d01550dff033a8d391a6f7772b4c99
mallky <makarkuz@yandex.ru>
2018-02-25 01:23:41 +0400

    (feature) put images into image folder in dist

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,12 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash].[ext]'
+            } 
+          }
         ]
       },
       {
