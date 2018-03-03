acc11573e49e9aa11a8ba1e8870c7d1234cb5304
mallky <makarkuz@yandex.ru>
2018-03-03 13:33:25 +0400

    (feature) add local ip

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const localIp = require('my-local-ip')();
  
+console.log(`Loacal IP is ${localIp}`);
 module.exports = merge(common, {   
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    host: localIp,
     hot: true
   },
   plugins: [
